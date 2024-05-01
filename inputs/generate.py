d96e9857e4a6d6252671b843fe23e30531bcc3e8
Miss_Bamboo <yunqingy@andrew.cmu.edu>
2024-05-01 14:14:13 -0400

    python script for generating clique

---
+++ inputs/generate.py
@@ -0,0 +1,18 @@
+# run this file to generate interesting custom inputs
+
+if __name__ == "__main__":
+    # Define the parameters
+    NAME = "clique3000"
+    num_nodes = 3000
+    num_edges = num_nodes * (num_nodes - 1)
+
+    with open(f"{NAME}.txt", "w") as file:
+        # Write header information
+        file.write(f"# Directed graph (each unordered pair of nodes is saved once): {NAME}.txt\n")
+        file.write(f"# Nodes: {num_nodes} Edges: {num_edges}\n")
+        file.write("# FromNodeId\tToNodeId\n")
+        
+        for i in range(num_nodes):
+            for j in range(num_nodes):
+                if i != j:
+                    file.write(f"{i}\t{j}\n")
